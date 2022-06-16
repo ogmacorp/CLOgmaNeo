075536d3f2b6c23ec31f9be9c719caed02df0d4e
Eric Laukien <ericlaukien@gmail.com>
2022-06-15 21:19:03 -0400

    Working

--- ogmaneo/__init__.py
+++ ogmaneo/__init__.py
@@ -4,12 +4,10 @@ import importlib_resources as res
 def load_prog(ctx: cl.Context, kernel_file: str = 'core.cl'):
     kernels_src = ''
 
-    with res.open_text('ogmaneo', 'kernels/' + kernel_file) as f:
+    with res.open_text('ogmaneo', kernel_file) as f:
         kernels_src = f.read()
 
-    prog = cl.Program(ctx, kernels_src).build()
-
-    return prog
+    return cl.Program(ctx, kernels_src)
 
 def load_prog_extra(ctx: cl.Context):
     return load_prog(ctx, 'extra.cl')
