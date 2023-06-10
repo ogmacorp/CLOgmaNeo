7963f3a4f80f238b7c5dae8953db1046786ec27e
Eric Laukien <ericlaukien@gmail.com>
2023-06-09 22:16:07 -0400

    Progress

--- ogmaneo/encoder.py
+++ ogmaneo/encoder.py
@@ -155,7 +155,7 @@ class Encoder:
 
                 vec_visible_size = np.array(list(vld.size), dtype=np.int32)
                 
-                self.encoder_accum_usages_kernel(cq, self.hidden_size, (1, 1, self.hidden_size[2]),
+                self.encoder_accum_usages_kernel(cq, (self.hidden_size[0], self.hidden_size[1], 1), None,
                         self.hidden_states.data, vl.usages.data, self.usage_sums.data,
                         vec_visible_size, vec_hidden_size, np.int32(vld.radius), np.int32(diam),
                         np.array([ vld.size[0] / self.hidden_size[0], vld.size[1] / self.hidden_size[1] ], dtype=np.float32),
