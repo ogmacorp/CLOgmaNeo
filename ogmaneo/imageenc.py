5b94851639bd7f0bfabf7cc0f05a01040775baa2
Eric Laukien <ericlaukien@gmail.com>
2025-07-20 08:04:12 -0400

    Tuning

--- ogmaneo/imageenc.py
+++ ogmaneo/imageenc.py
@@ -64,9 +64,9 @@ class ImageEnc:
                 self.vls.append(vl)
 
             # Hyperparameters
-            self.lr = 0.1
+            self.lr = 0.05
             self.rr = 0.05
-            self.falloff = 0.9
+            self.falloff = 0.99
             self.n_radius = 1
 
         else: # Load from h5py group
